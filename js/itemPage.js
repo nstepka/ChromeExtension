30e9c690a1f220c650eff6206b8a612ae4772391
Nicholas Stepka <40475549+nstepka@users.noreply.github.com>
2019-05-21 21:01:08 -0500

    Working
    Page pulls from chrome storage if sendOffers has been been updated.
    pulls from chrome storage the client username closet url and goes back

--- js/itemPage.js
+++ js/itemPage.js
@@ -1,32 +1,90 @@
 $(document).ready(function() {
-offerDiscount();
+//offerDiscount();
   var discountPercent = .9;
-})
+  wait(1000);
+  redirectBackToCloset();
 
+});
 
+function offerToLikers() {
+  // $("a.btn.white.private")[0].click();
+  // }
+  // function clickCalc() {
+  //   $("#offer_to_likers_calculator").each(function (index) { $(this).get(0).click() });
+  //   wait(1500);
+  //   $(".offer_button")[2].click();
+  //   wait(1500);
+  //
+  //   $('[data-pa-name="apply_offer"]')[0].click();
+  //   wait(1500);
+  //   $("#shipping-discount-selection")[0].click();
+  //   wait(1500);
+  //   document.querySelector("#bundle_offer_form > div.modal-body > div.offer-container > div > div > div:nth-child(3) > div > div.dropdown-menu > div:nth-child(1)").click();
+  //   wait(1000);
+  //   document.querySelector("#bundle_offer_form > div.modal-footer > button.btn.blue").click();
 
+    }
+function redirectBackToCloset() {
+  wait(4000);
+  chrome.storage.sync.get('sendOffers', function(offer) {
+     var sendOffers = offer.sendOffers;
+     alert(sendOffers);
+  chrome.storage.sync.get('closetPage', function(page) {
+  var closetPage = page.closetPage;
+
+  window.location.href = closetPage;
+
+  });
+  });
+}
 function offerDiscount() {
-wait(1000);
-//  alert($(".btn.white").first().text());
-     $(".btn.white").first().click();  //click price drop  not clicking
-     wait(10000);
-     alert("fucerk");
-     $("a.btn.white.private").click(); //click offer to likers
-     alert("fuck2");
-     wait(1000);
-     $(".icon.calculator").click(); //loads the calculate offers tab to get the current lising price_calculator
-     wait(1000);
-     var htmlPrice = document.getElementsByClassName("listing_price")[0].innerText;  //gets the price of the object
-     wait(1000);
-     var actualPrice = htmlPrice.replace("$", "");  //removes the $ from the price.
-     var discountPrice = actualPrice * .9;  //applies the discount
-     discountPrice = Math.floor(discountPrice);  //rounds the number down to the a whole number amount
-     $(".close")[3].click(); //closes the window where i got the price.
-     document.getElementById("offer_form_amount").value = discountPrice;//inserts the price.
-     document.querySelector(".select-shipping-label").click()  //selects teh discount drop_down
-     $(".d-ib")[0].click();  //click the $1.50 discount
-     $(".hide.modal.fade.expanded_offer_window.in").find(".btn.blue").click();  //SEND THE OFF
 
+//  alert($(".btn.white").first().text());
+    //var aTags = document.getElementsByClassName("btn white");
+    //var searchText = "Price Drop";
+    //var found = $('[data-pa-name="price_drop"]').text();
+
+  //  console.log(`${found} is a thing`)
+    $('[data-pa-name="price_drop"]')[0].click();
+
+    setTimeout(offerToLikers, 2000);
+    wait(1000);
+    setTimeout(clickCalc, 5000);
+
+
+
+
+
+
+
+    //------------------------^^^WORKING
+//    for (var i = 0; i < aTags.length; i++) {
+  //    if (aTags[i].textContent == searchText) {
+    //    found = aTags[i];
+  //      $(aTags).click();
+    //    console.log($(aTags));
+    //    break;
+  //    }
+  //  } //click price drop  not clicking
+//     wait(1000);
+     //alert("frick!");
+  //    $("a.btn.white.private").click(); //click offer to likers
+  //    console.log($("a.btn.white.private"));
+  //    //alert("darn it 2");
+  // //   wait(1000);
+  //    $(".icon.calculator").click(); //loads the calculate offers tab to get the current lising price_calculator
+  //   // wait(1000);
+  //    var htmlPrice = document.getElementsByClassName("listing_price")[0].innerText;  //gets the price of the object
+  //   // wait(1000);
+  //    var actualPrice = htmlPrice.replace("$", "");  //removes the $ from the price.
+  //    var discountPrice = actualPrice * .9;  //applies the discount
+  //    discountPrice = Math.floor(discountPrice);  //rounds the number down to the a whole number amount
+  //    $(".close")[3].click(); //closes the window where i got the price.
+  //    document.getElementById("offer_form_amount").value = discountPrice;//inserts the price.
+  //    document.querySelector(".shipping-discount-selection").click()  //selects teh discount drop_down
+  //   $("main.experiences-header:nth-child(7) div.hide.modal.fade.expanded_offer_window.in:nth-child(2) form.new_offer_form div.modal-body:nth-child(9) div.offer-container div.form-group div.seller_shipping_discount_offer div.col-form-control:nth-child(3) div.dropdown.open:nth-child(1) div.dropdown-menu > div.dropdown-item:nth-child(1)").click();  //click the $1.50 discount
+  //    $(".hide.modal.fade.expanded_offer_window.in").find(".btn.blue").click();  //SEND THE OFF
+  //    $("main.experiences-header:nth-child(7) div:nth-child(1) div.modal.fade.hide.in:nth-child(9) div.modal-body div.modal-footer > a.btn.blue").click();
  }
 
 
@@ -36,5 +94,6 @@ wait(1000);
     while(end < start + ms) {
       end = new Date().getTime();
    }
+
 
  }
