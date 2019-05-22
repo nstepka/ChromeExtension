c860bcf0f9757489e6151d68998b63b534b83b62
Nicholas Stepka <40475549+nstepka@users.noreply.github.com>
2019-05-21 20:59:04 -0500

    saved send offers, closet goes back to homepage

--- js/sendOffers.js
+++ js/sendOffers.js
@@ -1,9 +1,8 @@
 window.onload = function() {
-shareCloset();
-}
+getCloset();
+wait(2000);
+};
 
-
-  var sendOffers = 2;
   var items = [];
   var status = false;
   var url = "";
@@ -11,22 +10,52 @@ shareCloset();
   var num1 = 1;
     var discountPercent = .9;
 
+  //
+   function getCloset() {
+     //document.querySelector("#availability > li:nth-child(2) > label").click();
+     var closetPage = window.location.href;
+     chrome.storage.sync.set({'closetPage' : closetPage});
+     //window.location.href
+     chrome.storage.sync.get('sendOffers', function(offer) {
+       var sendOffers = offer.sendOffers;
+        alert(sendOffers);
+       var closet = [];
+       closet = $(".tile");
+       var www = "www.poshmark.com";
+       url = closet[0].firstChild.pathname;
+       var pageLink = www.concat(url);
+       window.location.href = url;
 
-  function shareCloset() {
-  	sendOffers = chrome.storage.local.get('sendOffers', function (items) {
-  	sendOffers = items.sendOffers;
-        var closet = [];
-  			closet = $(".tile");
-        var www = "www.poshmark.com";
-        url = closet[0].firstChild.pathname;
+     });
+   };
+  //   chrome.storage.sync.get('sendOffers', function(offer) {
+  //     alert(parseInt(offer.sendOffers));
+  //document.querySelector("#availability > li:nth-child(2) > label")
+  //   });
+    // alert(sendOffers);
+    //     var closet = [];
+  	// 		closet = $(".tile");
+    //     var www = "www.poshmark.com";
+    //     url = closet[0].firstChild.pathname;
+    //
+    //     var result = www.concat(url);
+    //     //alert(result);
+    //     wait(10000);
+    // // window.location.href  = closet[0].firstChild.pathname;
+    // //   chrome.tabs.executeScript({
+    // //     file: 'itemPage.js'
+    // //      });
+    //
+    //
+    // sendOffers = 2;
 
-        var result = www.concat(url);
-    window.location.href  = closet[0].firstChild.pathname;
-      chrome.tabs.executeScript({
-        file: 'itemPage.js'
-         });
 
 
-    sendOffers = 2;
-  });
+
+function wait(ms){
+   var start = new Date().getTime();
+   var end = start;
+   while(end < start + ms) {
+     end = new Date().getTime();
+  }
 }
