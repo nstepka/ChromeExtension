e1633ea8b22c3e4d26808518791f5882ebcd9dae
Nicholas Stepka <40475549+nstepka@users.noreply.github.com>
2019-05-21 21:01:57 -0500

    window onload switch
    saves if you click start sharing executes script

--- popup.js
+++ popup.js
@@ -1,68 +1,93 @@
-$(document).ready(function() {
+window.onload = function() {
+
+
+
   setUpPopUp();
-  sendOffer();
-})
+  offerScript();
+
+};
 
 
-console.log("ready to go");
-
-var minimumTime = 3;
-var maxShareTime = 5;
-var discountPercent = 10;
-var shareStatus = 0;
-var sendOffers = 0;
 //itemNumberInput
 
 function setUpPopUp() {
-  chrome.storage.local.get("minimumTime", function(fetchedData) {
-    $('#shareTimeMinimum')[0].value = minimumTime;
-    $('#shareTimeMaximum')[0].value = maxShareTime;
-    $('#discountPercent')[0].value = discountPercent;
-  });
+  var minimumTime = 3;
+  var maxShareTime = 5;
+  var discountPercent = 10;
+  var shareStatus = 0;
+  var numberOfItems = 1
+    $('#shareTimeMinimum').value = minimumTime;
+    $('#shareTimeMaximum').value = maxShareTime;
+    $('#discountPercent').value = discountPercent;
+    $('#numberOfItems').value = numberOfItems;
+
 
 
 
 }
 
+function offerScript () {
+$('#sendOffers').click(function() {
+  //numberOfItems = document.getElementById('numberOfItems').value;
+  //wont need if you click avability
+  //chrome.storage.sync.set({'numberOfItems' : numberOfItems});
 
-function startSharing() {
-    document.getElementById('startSharing').onclick = function() {
-
-     minimumTime = document.getElementById('shareTimeMinimum').value;
-     maxShareTime = document.getElementById('shareTimeMaximum').value;
-     shareStatus = 1;
-
-     chrome.storage.local.set({'minimumTime': minimumTime});
-     chrome.storage.local.set({'maxShareTime': maxShareTime});
-     chrome.storage.local.set({'shareStatus' : shareStatus});
-
-     chrome.tabs.executeScript({
-       file: 'js/itemshare.js'
-        });
+  sendOffers = 2;
+  chrome.storage.sync.set({'sendOffers' : sendOffers});
+  alert(sendOffers);
 
 
-     }
+
+  chrome.tabs.executeScript({
+    file: 'js/sendOffers.js'
+     });
+
+
+});
 }
-
-document.getElementById('startSharing').addEventListener('click', sendOffer);
-function sendOffer() {
-document.getElementById('sendOffers').onclick = function () {
-    discountPercent = document.getElementById('discountPercent').value;
-    chrome.storage.local.set({'discountPercent': discountPercent});
-    sendOffers = 1;
-    chrome.storage.local.set({'sendOffers': sendOffers});
-
-
-
-
-    chrome.tabs.executeScript({
-      file: 'js/sendOffers.js'
-       });
-
-
-
-
-
-  };
-
-}
+// function startSharing() {
+//     document.getElementById('startSharing').onclick = function() {
+//       var startingNumber = 0;
+//       chrome.storage.local.set({'startingNumber': startingNumber});
+//       numberOfItems = document.getElementById('numberOfItems').value;
+//      minimumTime = document.getElementById('shareTimeMinimum').value;
+//      maxShareTime = document.getElementById('shareTimeMaximum').value;
+//      numberOfItems = document.getElementById('numberOfItems').value;
+//      shareStatus = 1;
+//
+//      chrome.storage.local.set({'minimumTime': minimumTime});
+//      chrome.storage.local.set({'maxShareTime': maxShareTime});
+//      chrome.storage.local.set({'shareStatus' : shareStatus});
+//      chrome.storage.local.set({'numberOfItems' : numberOfItems});
+//
+//
+//      chrome.tabs.executeScript({
+//        file: 'js/itemshare.js'
+//         });
+//
+//
+//      }
+// }
+//
+// document.getElementById('startSharing').addEventListener('click', sendOffer);
+// function sendOffer() {
+// document.getElementById('sendOffers').onclick = function () {
+//     discountPercent = document.getElementById('discountPercent').value;
+//     chrome.storage.local.set({'discountPercent': discountPercent});
+//     sendOffers = 1;
+//     chrome.storage.local.set({'sendOffers': sendOffers});
+//
+//
+//
+//
+//     chrome.tabs.executeScript({
+//       file: 'js/sendOffers.js'
+//        });
+//
+//
+//
+//
+//
+//   };
+//
+// }
